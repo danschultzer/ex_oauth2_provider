3588ce06aee014d0dc921f674d7a11909751a49b
Dan Schultzer <dan@dreamconception.com>
2017-03-27 23:33:41 -0700

    Making travis happy

--- test/mix/tasks/ex_oauth2_provider.install_test.exs
+++ test/mix/tasks/ex_oauth2_provider.install_test.exs
@@ -25,7 +25,8 @@ defmodule Mix.Tasks.ExOauth2Provider.InstallTest do
   end
 
   setup do
-    File.rm_rf!(unquote(tmp_path))
+    File.rm_rf(unquote(tmp_path))
+    File.mkdir(unquote(tmp_path))
     :ok
   end
 
