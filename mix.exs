e6430d4e9ebb95725a3884d64fe6cdda76a169e5
Dan Schultzer <dan@dreamconception.com>
2018-08-01 11:54:38 -0700

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -37,14 +37,14 @@ defmodule ExOauth2Provider.Mixfile do
   defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
-    [{:ecto, "~> 2.1 or ~> 2.2"},
-     {:plug, "~> 1.0 or ~> 1.1 or ~> 1.2 or ~> 1.3"},
+    [{:ecto, ">= 2.1.0 or < 2.3.0"},
+     {:plug, ">= 1.0.0 and < 1.7.0"},
      {:jason, "~> 1.1"},
      {:postgrex, ">= 0.11.1", optional: true},
 
      # Dev and test dependencies
-     {:credo, "~> 0.9", only: [:dev, :test]},
-     {:ex_doc, "~> 0.18", only: :dev, runtime: false}]
+     {:credo, "~> 0.10", only: [:dev, :test]},
+     {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}]
   end
 
   defp package do
