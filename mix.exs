d13483bfea14bf5ce49b5f2f4838060f946e9737
Dan Schultzer <dan@dreamconception.com>
2019-05-08 08:25:31 -0700

    Clean up mix.exs

--- mix.exs
+++ mix.exs
@@ -4,26 +4,21 @@ defmodule ExOauth2Provider.Mixfile do
   @version "0.5.0"
 
   def project do
-    [app: :ex_oauth2_provider,
-     version: @version,
-     elixir: "~> 1.8",
-     elixirc_paths: elixirc_paths(Mix.env),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     preferred_cli_env: [credo: :test, ex_doc: :test],
-     deps: deps(),
+    [
+      app: :ex_oauth2_provider,
+      version: @version,
+      elixir: "~> 1.8",
+      elixirc_paths: elixirc_paths(Mix.env),
+      start_permanent: Mix.env == :prod,
+      deps: deps(),
 
-     # Hex
-     description: "No brainer OAuth 2.0 provider",
-     package: package(),
+      # Hex
+      description: "No brainer OAuth 2.0 provider",
+      package: package(),
 
-     # Docs
-     name: "ExOauth2Provider",
-     docs: [source_ref: "v#{@version}",
-            main: "ExOauth2Provider",
-            canonical: "http://hexdocs.pm/ex_oauth2_provider",
-            source_url: "https://github.com/danschultzer/ex_oauth2_provider",
-            extras: ["README.md"]]
+      # Docs
+      name: "ExOauth2Provider",
+      docs: docs()
     ]
   end
 
@@ -31,22 +26,25 @@ defmodule ExOauth2Provider.Mixfile do
     [extra_applications: extra_applications(Mix.env)]
   end
 
-  defp extra_applications(:test), do: [:postgrex, :ecto, :logger]
+  defp extra_applications(:test), do: [:ecto, :logger]
   defp extra_applications(_), do: [:logger]
 
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   defp deps do
-    [{:ecto, "~> 3.0"},
-     {:plug, ">= 1.0.0 and < 1.8.0"},
+    [
+      {:ecto, "~> 3.0"},
+      {:plug, ">= 1.0.0 and < 1.8.0"},
 
-     # Dev and test dependencies
-     {:credo, "~> 0.10", only: [:dev, :test]},
-     {:ex_doc, ">= 0.0.0", only: :dev},
-     {:ecto_sql, "~> 3.0.0", only: [:test]},
-     {:plug_cowboy, "~> 2.0", only: [:test]},
-     {:postgrex, "~> 0.14", only: :test}]
+      # Dev and test dependencies
+      {:credo, "~> 0.10", only: [:dev, :test]},
+
+      {:ex_doc, ">= 0.0.0", only: :dev},
+
+      {:ecto_sql, "~> 3.0.0", only: :test},
+      {:plug_cowboy, "~> 2.0", only: :test},
+      {:postgrex, "~> 0.14", only: :test}]
   end
 
   defp package do
@@ -55,6 +53,16 @@ defmodule ExOauth2Provider.Mixfile do
       licenses: ["MIT"],
       links: %{github: "https://github.com/danschultzer/ex_oauth2_provider"},
       files: ~w(lib LICENSE mix.exs README.md)
+    ]
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      main: "ExOauth2Provider",
+      canonical: "http://hexdocs.pm/ex_oauth2_provider",
+      source_url: "https://github.com/danschultzer/ex_oauth2_provider",
+      extras: ["README.md"]
     ]
   end
 end
